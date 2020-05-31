d7c787bc349876bbe65f591cfff6574337133856
Kevin <kevin.zamperetti@dbccompany.com.br>
2020-05-30 22:39:08 -0300

    init

---
+++ build/precache-manifest.3051c664d49c75ea4eb153837e2020a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d600b3a37d492d195db7e674ec3a68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e355d31d7aa95b26d15d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3c338a78d3c2da2ed55",
+    "url": "/static/js/2.3b3f0d9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b3f0d9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e355d31d7aa95b26d15d",
+    "url": "/static/js/main.bc37b24d.chunk.js"
+  },
+  {
+    "revision": "958e31ef2c1025ea683d",
+    "url": "/static/js/runtime-main.807cba50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
